5dbb4efe48afad21b93ccedfbce4eb7b686f055e
Franco Ortega <francopdx@gmail.com>
2020-11-16 13:49:15 -0800

    add initial buttons and links to Header.js and add Header.css

--- src/Header.js
+++ src/Header.js
@@ -7,10 +7,39 @@ import {
     Switch,
     Link
 } from "react-router-dom";
+import './Header.css';
 
 export default class Header extends Component {
     
     render() {
-        return (<div></div>)
+        return (
+        <div className='header-div'>
+            <div className='header-search-div'>
+            <Link to='/search'>
+                <button className='header-search-button'>
+                    Search
+                </button>
+            </Link>
+            </div>
+            <div className='header-links-div'>
+                <Link to='/'>Home</Link><br />
+                <Link to='/favorites'>Favorites 🌟</Link><br />
+                <Link to='/about'>About Us 👋</Link><br />
+                <Link to='/test'>Test</Link>
+            </div>
+            <div className='header-sign-div'>
+                <Link to='/signup'>
+                    <button className='header-signup-button'>
+                        Sign Up
+                    </button>
+                </Link>
+                <Link to='/signin'>
+                    <button className='header-signin-button'>
+                        Sign In
+                    </button>
+                </Link>
+            </div>
+        </div>
+        )
     }
 }
