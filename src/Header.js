2840d2ff1090a7e3f3f891b242da69269f143d85
Franco Ortega <francopdx@gmail.com>
2020-11-16 15:32:59 -0800

    comment out fakeHandleSignIn

--- src/Header.js
+++ src/Header.js
@@ -12,56 +12,70 @@ import './Header.css';
 export default class Header extends Component {
 
     state = {
-        token: 'token'
+        token: localStorage.getItem('TOKEN') || ''
     }
 
-    logOut = () => {
+    handleLogOut = () => {
+        localStorage.setItem('TOKEN', '');
         this.setState({ token: '' })
     }
+
+    // fakeHandleSignIn = () => {
+    //     this.setState({ token: 'token' })
+    // }
     
     render() {
         return (
-        <div className='header-div'>
-            <div className='header-search-div'>
-            <Link to='/search'>
-                <button className='header-search-button'>
-                    Search
-                </button>
-            </Link>
-            </div>
-            <div className='header-links-div'>
-                <Link to='/'>Home</Link>
-                <Link to='/about'>About Us 👋</Link>
-            </div>
-            <div className='favorites-div'>
-                { this.state.token && <Link to='/favorites' className='favorites-link'>🌟</Link> }
-                
-            </div>
-            <div className='header-sign-div'>
-            { !this.state.token && 
-                <Link to='/signup'>
-                    <button className='header-signup-button'>
-                        Sign Up
+            <div className='header-div'>
+
+                {/* Search Button links to Search.js */}
+                <div className='header-search-div'>
+                <Link to='/search'>
+                    <button className='header-search-button'>
+                        Search
                     </button>
                 </Link>
-            }
-            { !this.state.token && 
-                <Link to='/signin'>
-                    <button className='header-signin-button'>
-                        Sign In
-                    </button>
-                </Link>
-            }
-                <div className='header-logout-div'>
-                { this.state.token &&
-                <Link to='/'>
-                    <button
-                     onClick={this.logOut}
-                     className='header-logout-button'>
-                        Log Out
-                    </button>
-                </Link>
-                }
+                </div>
+
+                {/* Header text links (non-button) */}
+                <div className='header-links-div'>
+                    <Link to='/'>Home</Link>
+                    <Link to='/about'>About Us 👋</Link>
+                </div>
+
+                {/* Favorites "STAR ICON" (when logged in) */}
+                <div className='favorites-div'>
+                    { this.state.token && <Link to='/favorites' className='favorites-link'>🌟</Link> }
+                </div>
+
+                {/* Sign Up/Sign In and Log Out buttons */}
+                <div className='header-sign-div'>
+                    { !this.state.token && 
+                        <Link to='/signup'>
+                            <button className='header-signup-button'>
+                                Sign Up
+                            </button>
+                        </Link>
+                    }
+                    { !this.state.token && 
+                        <Link to='/signin'>
+                            <button
+                            // onClick={this.fakeHandleSignIn}
+                            className='header-signin-button'>
+                                Sign In
+                            </button>
+                        </Link>
+                    }
+                    <div className='header-logout-div'>
+                    { this.state.token &&
+                    <Link to='/'>
+                        <button
+                        onClick={this.handleLogOut}
+                        className='header-logout-button'>
+                            Log Out
+                        </button>
+                    </Link>
+                    }
                 </div>
             </div>
         </div>
