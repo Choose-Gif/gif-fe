45b5e8128b8fcf432fe86e3c96c0d2685bd384f7
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2020-11-16 15:48:01 -0500

    gifs rendering on search results page

--- src/Search.js
+++ src/Search.js
@@ -1,32 +1,30 @@
 import React, { Component } from 'react';
-import request from 'superagent';
-import Header from './Header.js';
+import './Search.css';
+
 
 export default class Search extends Component {
-
-    componentDidMount = async () => {
-     
-    }
 
     render() {
         return (
             <div>
-                Search Page
-                    <form onSubmit={this.props.handleSubmit}>
-                        <input 
-                            value={this.props.query} 
-                            onChange={this.props.handleInput}/>
-                        <button>Submit</button>
-                    </form>
+                <h2>Search Page</h2>
+                <form onSubmit={this.props.handleSubmit}>
+                    <input 
+                        value={this.props.query} 
+                        onChange={this.props.handleInput}/>
+                    <button>Submit</button>
+                </form>
+                <div className="group">
                 {
                     this.props.searchResults.map(oneItem => 
-                        <div>
-                            Title: {oneItem.title}
+                        <div classname="individual" key={`${oneItem.title}${Math.random()}`}>
+                            <img className="item-picture" src={oneItem.images.original.url} alt={oneItem.title}></img>
+                            <p className="item-text">Title: {oneItem.title}</p>
+                            {/* <p className="item-text">Url: {oneItem.images.original.url}</p> */}
                         </div>
                     )
                 }
-
-
+                </div>
             </div>
         )
     }
