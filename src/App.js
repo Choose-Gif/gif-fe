e2903755d78dcf3c0c844b0324cbf89bcbfa978b
kevinfiero <kjf46@cornell.edu>
2020-11-18 22:11:54 -0800

    clear search bar, click text on trending images

--- src/App.js
+++ src/App.js
@@ -32,8 +32,7 @@ export default class App extends Component {
   handleSubmit = async e => {
     const response = await request
    .get(`https://choose-gif-be.herokuapp.com/search?query=${this.state.query}`)
-   this.setState({ searchResults: response.body.data });
-    //KEEP THE LINE BELOW THIS COMMENT
+   this.setState({ searchResults: response.body.data, query: '' });
     if(this.state.token!==''){
       await this.fetchFavorites();
     }
@@ -102,6 +101,7 @@ export default class App extends Component {
         <Router>
         <Route render={(routerProps) => <Header         
             token={this.state.token}
+            query={this.state.query}
             handleLogOut={this.handleLogOut}
             handleSubmit={this.handleSubmit}
             handleTokenChange={this.handleTokenChange} 
